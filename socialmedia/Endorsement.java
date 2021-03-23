bea8d7db0c0167d637f2cc219530db6fe2d4a687
JessicaEdmonds <81162316+JessicaEdmonds@users.noreply.github.com>
2021-03-23 12:06:26 +0000

    populating endorsement class

--- socialmedia/Endorsement.java
+++ socialmedia/Endorsement.java
@@ -1,3 +1,19 @@
-public class Endorsement {
-    
+public class Endorsement extends Post {
+
+    // inherits message from post class
+
+    Post refersTo = new Post();
+
+    public Post getRefersTo() {
+        return refersTo;
+    }
+
+    public void setRefersTo(Post refersTo) {
+        this.refersTo = refersTo;
+    }
+
+    public Endorsement(int postID, String author, ArrayList<postRecord> records, Post refersTo) {
+        super(postID, author, records);
+        this.refersTo = refersTo;
+    }
 }
