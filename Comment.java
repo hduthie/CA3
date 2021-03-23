c58c4713f32d8ae7e7eafbbd725fc629ec843141
hduthie <73220715+hduthie@users.noreply.github.com>
2021-03-23 11:53:18 +0000

    SAving interfaces to packages

--- Comment.java
+++ Comment.java
@@ -1,9 +1,19 @@
 
 // comment extends Post
-public class Comment {
+public class Comment extends Post{
     // inherits ID from post
     // inherits message
     // inherits author ( an account type)
     Post replyingTo; // the comment or original post this comment is replying to
 
+    public Post getReplyingTo() {
+        return replyingTo;
+    }
+
+    public void setReplyingTo(Post replyingTo) {
+        this.replyingTo = replyingTo;
+    }
+
+
+    
 }
--- SocialMedia.java
+++ SocialMedia.java
@@ -1,3 +1,23 @@
-public class SocialMedia {
-    
+package socialmedia;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class SocialMedia extends Serializable{
+
+    int createAccount(String handle) throws IllegalHandleException, InvalidHandleException;
+
+	/**
+	 * The method removes the account with the corresponding ID from the platform.
+	 * When an account is removed, all of their posts and likes should also be
+	 * removed.
+	 * <p>
+	 * The state of this SocialMediaPlatform must be be unchanged if any exceptions
+	 * are thrown.
+	 * 
+	 * @param id ID of the account.
+	 * @throws AccountIDNotRecognisedException if the ID does not match to any
+	 *                                         account in the system.
+	 */
+
 }
